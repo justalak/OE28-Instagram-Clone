c91daab77e3601569d5bda603d8bf980884bf34e
Việt Hoàng <justalak29@gmail.com>
2020-03-05 11:11:57 +0700

    Remember me

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   enum gender: {female: 0, male: 1}
   enum role: {user: 0, admin: 1}
 
@@ -17,17 +18,48 @@ class User < ApplicationRecord
     uniqueness: {case_sensitive: true}
   validates :password, presence: true,
     length: {minimum: Settings.user.min_length_password}, allow_nil: true
-  
+
   before_save :downcase_email
 
   has_secure_password
-  
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def authenticate? attribute, token
+    digest = send "#{attribute}_digest"
+    return false unless token
+
+    BCrypt::Password.new(digest).is_password? token
+  end
+
+  def forget
+    update remember_digest: nil
+  end
+
   def default_avatar_image
     Settings.user.default_avt
   end
-  
+
   private
-  
+
   def downcase_email
     email.downcase!
   end
