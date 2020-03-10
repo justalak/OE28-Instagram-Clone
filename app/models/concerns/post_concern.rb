46875065538b53b2b412ee8b4f9bb7fa17493aae
Việt Hoàng <justalak29@gmail.com>
2020-03-10 22:12:27 +0700

    add hashtag to post description

--- app/models/concerns/post_concern.rb
+++ app/models/concerns/post_concern.rb
@@ -5,4 +5,17 @@ module PostConcern
     post_params.except :images if post_params[:images].blank?
     update post_params
   end
+
+  def extract_name_hashtags
+    description.to_s.scan(/#\w+/).map{|name| name.gsub("#", "")}
+  end
+
+  def add_hashtags
+    extract_name_hashtags.each do |name|
+      hashtag = Hashtag.find_by name: name
+      hashtag ||= Hashtag.create name: name
+
+      hashtags << hashtag
+    end
+  end
 end
