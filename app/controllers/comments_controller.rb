77dbc53501abb78a2905378f7c8c9fa74f74225c
ChungPhamVan <chung.pv172983@sis.hust.edu.vn>
2020-03-17 11:48:09 +0700

    edit_delete_comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
-  before_action :logged_in_user, :load_post, only: :create
+  before_action :logged_in_user, :load_post
+  before_action :load_comment, :correct_user, except: :create
 
   def create
     @comment = @post.comments.build comment_params
@@ -15,12 +16,49 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @comment.update comment_params
+      flash[:success] = t ".update_success"
+      redirect_to @post
+    else
+      flash.now[:danger] = t ".update_fail"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @comment.destroy
+      flash[:success] = t ".destroy_success"
+    else
+      flash[:danger] = t ".destroy_failed"
+    end
+    redirect_back fallback_location: root_path
+  end
+
   private
+
   def load_post
     @post = Post.find_by id: params[:post_id]
     return if @post
 
     flash[:danger] = t ".post_not_found"
+    redirect_back fallback_location: root_path
+  end
+
+  def load_comment
+    @comment = @post.comments.find_by id: params[:id]
+    return if @comment
+
+    flash[:danger] = t ".comment_not_found"
+    redirect_back fallback_location: root_path
+  end
+
+  def correct_user
+    return if current_user? @comment.user
+
+    flash[:danger] = t "access_denied"
     redirect_back fallback_location: root_path
   end
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,14 +6,14 @@ class Comment < ApplicationRecord
   belongs_to :parent, class_name: Comment.name, optional: true
 
   has_many :replies, class_name: Comment.name, foreign_key: :parent_id,
-    dependent: :destroy,
-    inverse_of: true
+    dependent: :destroy
 
   validates :content, presence: true
   validates :user_id, presence: true
   validates :post_id, presence: true
 
   delegate :username, to: :user, prefix: :user
+  delegate :user, to: :post, prefix: :post_owner
 
   scope :order_by_created_at, ->{order :created_at}
   scope :top, (lambda do |post_id|
